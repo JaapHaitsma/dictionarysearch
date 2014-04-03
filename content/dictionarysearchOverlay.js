107238d1fc7f4e1f12bb7188407592636af61bb7
Jaap A. Haitsma <jaap@haitsma.org>
2014-04-03 00:07:56 -0300

    Update logging Fix nl-NL translation Bump to version 28.0.0

--- content/dictionarysearchOverlay.js
+++ content/dictionarysearchOverlay.js
@@ -39,231 +39,246 @@ if(!org.mozdev.dictionarysearch) org.mozdev.dictionarysearch={};
 
 
 org.mozdev.dictionarysearch = {
-DEBUG: false,
-NUM_DICTIONARIES: 6,
-stringBundle: null,
-prefs: null,
-APP_NAME: "DictionarySearch",
-VERSION: "10.0.0",
-inThunderbird: false,
+    DEBUG: true,
+    NUM_DICTIONARIES: 6,
+    stringBundle: null,
+    prefs: null,
+    APP_NAME: "DictionarySearch",
+    VERSION: "28.0.0",
+    inThunderbird: false,
 
-dumpObject: function(obj) {
-    for(i in obj){         
-        this.debug(i + " = " + obj[i] + "\n");
-    }
-},
-
-debug: function(str) {
-    if (!this.DEBUG) {
-        return;
-    }
-    consoleService = Components.classes["@mozilla.org/consoleservice;1"]
-        .getService(Components.interfaces.nsIConsoleService);
-    consoleService.logStringMessage(this.APP_NAME + ": " + str);
-},
-
-init: function () {
-	this.prefs = Components.classes["@mozilla.org/preferences-service;1"].
-                        getService(Components.interfaces.nsIPrefService).getBranch("dictionarysearch.");
-
-	if (!this.prefs.prefHasUserValue("debug")) {
-		this.debug("init(): no preferences found in prefs.js taking default for debug");
-		this.prefs.setBoolPref("debug", false);
-	} else {
-		this.DEBUG = this.prefs.getBoolPref("debug");
-    }
-
-    this.debug("init()");
-
-	this.inThunderbird = false;
-	this.debug ("userAgent: " + navigator.userAgent);
-    if (navigator.userAgent.search(/Thunderbird/gi) != -1){
-        this.inThunderbird = true;
-    } 
-	this.debug("inThunderbird: " + this.inThunderbird);
-
-
-    this.stringBundle = document.getElementById("dictionarysearch_string_bundle");
-
-    if (!this.prefs.prefHasUserValue("menutext1")){
-        this.debug ("init(): no preferences found taking defaults");
-        // Assign default values
-        this.prefs.setCharPref("url1", this.stringBundle.getString("dictionarysearch.default.URL"));
-        this.prefs.setCharPref("menutext1", this.stringBundle.getString("dictionarysearch.default.menutext"));
-    }
-    for (i = 2; i <= this.NUM_DICTIONARIES; i++) {
-        if (!this.prefs.prefHasUserValue("menutext"+i)) {
-            this.prefs.setCharPref("url"+i, "");
-            this.prefs.setCharPref("menutext"+i, "");
+    dumpObject: function(obj) {
+        for(i in obj){         
+            this.debug(i + " = " + obj[i] + "\n");
         }
-    }            
-    if (document.getElementById("contentAreaContextMenu")) {
-        document.getElementById("contentAreaContextMenu").addEventListener("popupshowing", function () {org.mozdev.dictionarysearch.popup()}, false);
-    } else if (document.getElementById("messagePaneContext")){
-        document.getElementById("messagePaneContext").addEventListener("popupshowing", function () {org.mozdev.dictionarysearch.popup()}, false);    
-    } else if (document.getElementById("mailContext")){
-        document.getElementById("mailContext").addEventListener("popupshowing", function () {org.mozdev.dictionarysearch.popup()}, false);    
-    } else if (document.getElementById("msgComposeContext")){
-        document.getElementById("msgComposeContext").addEventListener("popupshowing", function () {org.mozdev.dictionarysearch.popup()}, false);    
-    }
+    },
 
-    if (!this.prefs.prefHasUserValue("version") || this.prefs.getCharPref("version") != this.VERSION) {
-		this.debug("init(): no version found or newer version");
-		url = "http://dictionarysearch.mozdev.org/thanks.html";
-		if (!this.inThunderbird) {       
-			setTimeout(function() { window.openUILinkIn(url, "tab"); }, 500);
-		} else {
-        	var messenger = Components.classes["@mozilla.org/messenger;1"].createInstance();         
-        	messenger = messenger.QueryInterface(Components.interfaces.nsIMessenger);
-        	messenger.launchExternalURL(url);         
-		}
-		this.prefs.setCharPref("version", this.VERSION);
-    }
-},
-
-popup: function() { 
-    // hide the Dictionary Search item when apropriate (use same logic as for web search)
-    this.debug("popup" + this.prefs);
-    var selectedText = this.getSelectedText(" ");
-       
-    if (selectedText!=""){
-        this.debug("popup() text selected");
-        if (selectedText.length > 18){
-            selectedText = selectedText.substr(0,14) + "...";
+    debug: function(str) {
+        if (!this.DEBUG) {
+            return;
         }
-        var menuText;
-        var item;
-
-        for (i=1 ;i<=this.NUM_DICTIONARIES; i++){
-            menuText = this.prefs.getCharPref("menutext"+i);
-            item = document.getElementById("context_dictionarysearchselect"+i);
-            item.hidden = true;
-            if (menuText!=""){        
-                item.hidden = false;        
-                menuText = menuText.split("$");
-                menuText = menuText[0] + selectedText + menuText[1];
-                item.setAttribute("label", menuText);
-            }
+        if (navigator.userAgent.search(/Thunderbird/gi) != -1){ // In Thunderbird
+            var consoleService = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
+            consoleService.logStringMessage(this.APP_NAME + ": " + str);
+        } else {
+            var console = Components.utils.import("resource://gre/modules/devtools/Console.jsm", {}).console;
+            console.log(this.APP_NAME + ": " + str);            
         }
-    }
-    else{
+    },
 
-       for (i=1 ;i<=this.NUM_DICTIONARIES; i++){
-            item = document.getElementById("context_dictionarysearchselect"+i);
-            item.hidden = true;
-       }
-    }
-},
-
-search: function(n){ 
-    var url = this.prefs.getCharPref("url"+n);
-    url = url.split("$");
-    url = url[0] + this.getSelectedText("+").toLocaleLowerCase() + url[1];
-    this.debug ("search() : url = " + url);
-
-    if (!this.inThunderbird){
-        openNewTabWith(url, null, null, true);
-    }
-    else{
-        var messenger = Components.classes["@mozilla.org/messenger;1"].createInstance();         
-        messenger = messenger.QueryInterface(Components.interfaces.nsIMessenger);
-		this.debug(messenger);
-		this.debug("launchExternalURL: " + url);
-        messenger.launchExternalURL(url);         
-    }
-},
-
-
-getSelectedText: function(concationationChar){
-    this.debug("getSelectedText()");
-    
-    var node = document.popupNode;
-    var selection = "";
-
-    if (node instanceof HTMLTextAreaElement || node instanceof HTMLInputElement && node.type == "text") {
-        selection = node.value.substring(node.selectionStart, node.selectionEnd);
-    } 
-    else {
-        var focusedWindow = new XPCNativeWrapper(document.commandDispatcher.focusedWindow, 'document', 'getSelection()');
-        selection = focusedWindow.getSelection().toString();
-    }
-
-    // Limit length to 150 to optimize performance. Longer does not make sense
-    if (selection.length>=150){
-        selection = selection.substring(0, 149);
-    }
-    selection = selection.replace(/(\n|\r|\t)+/g, " ");
-    // Strip spaces at start and end.
-    selection = selection.replace(/(^\s+)|(\s+$)/g, "");
-
-    selection = selection.split(" ");
-   
-    // Remove certain characters at the beginning and end of every word
-    for (i=0; i<selection.length; i++){
-        selection[i]=selection[i].replace(/^(\&|\(|\)|\[|\]|\{|\}|"|,|\.|!|\?|'|:|;)+/, "");
-        selection[i]=selection[i].replace(/(\&|\(|\)|\[|\]|\{|\}|"|,|\.|!|\?|'|:|;)+$/, "");
-    }   
-    selection = selection.join(concationationChar);   
-    return selection; 
-},
-
-saveSettings: function(){    
-    this.debug("saveSettings()");
-
-    var url;
-    var menuText;
-    var accessKey;
-    for (i=1; i<=this.NUM_DICTIONARIES; i++){
-        url = document.getElementById("dictionarysearchURL"+i).value;
-        menuText = document.getElementById("dictionarysearchMenuText"+i).value;
-        if (menuText != ""){       
-            if (!this.checkDollarSign(menuText)){
-                alert(this.stringBundle.getString("dictionarysearch.menuText.error1")  + " " + i
-                      + " " + this.stringBundle.getString("dictionarysearch.menuText.error2"));
-                return false;
-            }
-
-            if (!this.checkDollarSign(url)){
-                alert(this.stringBundle.getString("dictionarysearch.URL.error1") + " " + i
-                  + " " + this.stringBundle.getString("dictionarysearch.URL.error2"));
-                return false;
-            }
-
+    openURL: function(url){
+        if (!this.inThunderbird){       
+            setTimeout(function() { window.openUILinkIn(url, "tab"); }, 500);
+        } else {
+            var messenger = Components.classes["@mozilla.org/messenger;1"].createInstance();         
+            messenger = messenger.QueryInterface(Components.interfaces.nsIMessenger);
+            messenger.launchExternalURL(url);         
         }
-        this.prefs.setCharPref("url"+i, url);
-        this.prefs.setCharPref("menutext"+i, menuText);
-    }
-    return true;
-},
+    },
 
-loadSettings: function(){
-    this.debug("loadSettings()");
-    // We need to set the prefs and string bundle again, because we get another instance of the object in the pref dialog :-(
-    this.prefs = Components.classes["@mozilla.org/preferences-service;1"].
+    init: function(){
+    	this.prefs = Components.classes["@mozilla.org/preferences-service;1"].
                             getService(Components.interfaces.nsIPrefService).getBranch("dictionarysearch.");
 
-    this.stringBundle = document.getElementById("dictionarysearch_string_bundle");
+    	if (!this.prefs.prefHasUserValue("debug")) {
+    		this.debug("init(): no preferences found in prefs.js taking default for debug");
+    		this.prefs.setBoolPref("debug", false);
+    	} else {
+    		this.DEBUG = this.prefs.getBoolPref("debug");
+        }
+
+        this.debug("init()");
+
+    	this.inThunderbird = false;
+    	this.debug ("userAgent: " + navigator.userAgent);
+        if (navigator.userAgent.search(/Thunderbird/gi) != -1){
+            this.inThunderbird = true;
+        } 
+    	this.debug("inThunderbird: " + this.inThunderbird);
 
 
-    for (i=1; i<=this.NUM_DICTIONARIES; i++){
-        document.getElementById("dictionarysearchURL"+i).value = this.prefs.getCharPref("url"+i);
-        document.getElementById("dictionarysearchMenuText"+i).value = this.prefs.getCharPref("menutext"+i);
+        this.stringBundle = document.getElementById("dictionarysearch_string_bundle");
+
+
+        if (!this.prefs.prefHasUserValue("menutext1") || this.prefs.getCharPref("menutext1") == ""){
+            this.debug ("init(): no preferences found taking defaults");
+            // Assign default values
+            this.prefs.setCharPref("url1", this.stringBundle.getString("dictionarysearch.default.URL"));
+            this.prefs.setCharPref("menutext1", this.stringBundle.getString("dictionarysearch.default.menutext"));
+        }
+        for (var i = 2; i <= this.NUM_DICTIONARIES; i++) {
+            if (!this.prefs.prefHasUserValue("menutext"+i)) {
+                this.prefs.setCharPref("url"+i, "");
+                this.prefs.setCharPref("menutext"+i, "");
+            }
+        }            
+        if (document.getElementById("contentAreaContextMenu")) {
+            document.getElementById("contentAreaContextMenu").addEventListener("popupshowing", function () {org.mozdev.dictionarysearch.popup()}, false);
+        } else if (document.getElementById("messagePaneContext")){
+            document.getElementById("messagePaneContext").addEventListener("popupshowing", function () {org.mozdev.dictionarysearch.popup()}, false);    
+        } else if (document.getElementById("mailContext")){
+            document.getElementById("mailContext").addEventListener("popupshowing", function () {org.mozdev.dictionarysearch.popup()}, false);    
+        } else if (document.getElementById("msgComposeContext")){
+            document.getElementById("msgComposeContext").addEventListener("popupshowing", function () {org.mozdev.dictionarysearch.popup()}, false);    
+        }
+
+        if (!this.prefs.prefHasUserValue("version")) {
+            this.debug("init(): no version found");
+            this.openURL ("http://dictionarysearch.mozdev.org/thanks-install.html");
+            this.prefs.setCharPref("version", this.VERSION);
+        }
+        if (this.prefs.getCharPref("version") != this.VERSION) {
+            this.debug("init(): newer version");
+            this.openURL ("http://dictionarysearch.mozdev.org/thanks.html");
+            this.prefs.setCharPref("version", this.VERSION);
+        }
+
+    },
+
+    popup: function() { 
+        // hide the Dictionary Search item when apropriate (use same logic as for web search)
+        this.debug("popup" + this.prefs);
+        var selectedText = this.getSelectedText(" ");
+           
+        if (selectedText!=""){
+            this.debug("popup() text selected");
+            if (selectedText.length > 18){
+                selectedText = selectedText.substr(0,14) + "...";
+            }
+            var menuText;
+            var item;
+
+            for (i=1 ;i<=this.NUM_DICTIONARIES; i++){
+                menuText = this.prefs.getCharPref("menutext"+i);
+                item = document.getElementById("context_dictionarysearchselect"+i);
+                item.hidden = true;
+                if (menuText!=""){        
+                    item.hidden = false;        
+                    menuText = menuText.split("$");
+                    menuText = menuText[0] + selectedText + menuText[1];
+                    item.setAttribute("label", menuText);
+                }
+            }
+        }
+        else{
+
+           for (i=1 ;i<=this.NUM_DICTIONARIES; i++){
+                item = document.getElementById("context_dictionarysearchselect"+i);
+                item.hidden = true;
+           }
+        }
+    },
+
+    search: function(n){ 
+        var url = this.prefs.getCharPref("url"+n);
+        url = url.split("$");
+        url = url[0] + this.getSelectedText("+").toLocaleLowerCase() + url[1];
+        this.debug ("search() : url = " + url);
+
+        if (!this.inThunderbird){
+            openNewTabWith(url, null, null, true);
+        }
+        else{
+            var messenger = Components.classes["@mozilla.org/messenger;1"].createInstance();         
+            messenger = messenger.QueryInterface(Components.interfaces.nsIMessenger);
+    		this.debug(messenger);
+    		this.debug("launchExternalURL: " + url);
+            messenger.launchExternalURL(url);         
+        }
+    },
+
+
+    getSelectedText: function(concationationChar){
+        this.debug("getSelectedText()");
+        
+        var node = document.popupNode;
+        var selection = "";
+
+        if (node instanceof HTMLTextAreaElement || node instanceof HTMLInputElement && node.type == "text") {
+            selection = node.value.substring(node.selectionStart, node.selectionEnd);
+        } 
+        else {
+            var focusedWindow = new XPCNativeWrapper(document.commandDispatcher.focusedWindow, 'document', 'getSelection()');
+            selection = focusedWindow.getSelection().toString();
+        }
+
+        // Limit length to 150 to optimize performance. Longer does not make sense
+        if (selection.length>=150){
+            selection = selection.substring(0, 149);
+        }
+        selection = selection.replace(/(\n|\r|\t)+/g, " ");
+        // Strip spaces at start and end.
+        selection = selection.replace(/(^\s+)|(\s+$)/g, "");
+
+        selection = selection.split(" ");
+       
+        // Remove certain characters at the beginning and end of every word
+        for (i=0; i<selection.length; i++){
+            selection[i]=selection[i].replace(/^(\&|\(|\)|\[|\]|\{|\}|"|,|\.|!|\?|'|:|;)+/, "");
+            selection[i]=selection[i].replace(/(\&|\(|\)|\[|\]|\{|\}|"|,|\.|!|\?|'|:|;)+$/, "");
+        }   
+        selection = selection.join(concationationChar);   
+        return selection; 
+    },
+
+    saveSettings: function(){    
+        this.debug("saveSettings()");
+
+        var url;
+        var menuText;
+        var accessKey;
+        for (i=1; i<=this.NUM_DICTIONARIES; i++){
+            url = document.getElementById("dictionarysearchURL"+i).value;
+            menuText = document.getElementById("dictionarysearchMenuText"+i).value;
+            if (menuText != ""){       
+                if (!this.checkDollarSign(menuText)){
+                    alert(this.stringBundle.getString("dictionarysearch.menuText.error1")  + " " + i
+                          + " " + this.stringBundle.getString("dictionarysearch.menuText.error2"));
+                    return false;
+                }
+
+                if (!this.checkDollarSign(url)){
+                    alert(this.stringBundle.getString("dictionarysearch.URL.error1") + " " + i
+                      + " " + this.stringBundle.getString("dictionarysearch.URL.error2"));
+                    return false;
+                }
+
+            }
+            this.prefs.setCharPref("url"+i, url);
+            this.prefs.setCharPref("menutext"+i, menuText);
+        }
+        return true;
+    },
+
+    loadSettings: function(){
+        this.debug("loadSettings()");
+        // We need to set the prefs and string bundle again, because we get another instance of the object in the pref dialog :-(
+        this.prefs = Components.classes["@mozilla.org/preferences-service;1"].
+                                getService(Components.interfaces.nsIPrefService).getBranch("dictionarysearch.");
+
+        this.stringBundle = document.getElementById("dictionarysearch_string_bundle");
+
+
+        for (i=1; i<=this.NUM_DICTIONARIES; i++){
+            document.getElementById("dictionarysearchURL"+i).value = this.prefs.getCharPref("url"+i);
+            document.getElementById("dictionarysearchMenuText"+i).value = this.prefs.getCharPref("menutext"+i);
+        }
+    },
+
+    checkDollarSign: function(str){    
+        this.debug("checkDollarSign(str)");
+
+        ind = str.indexOf('$');
+        if (ind == -1){
+            return false;
+        }
+        if (ind != str.lastIndexOf('$')){
+            return false;
+        }
+        return true;
     }
-},
-
-checkDollarSign: function(str){    
-    this.debug("checkDollarSign(str)");
-
-    ind = str.indexOf('$');
-    if (ind == -1){
-        return false;
-    }
-    if (ind != str.lastIndexOf('$')){
-        return false;
-    }
-    return true;
-}
 };
+
 
 // Every time a new browser window is made init will be called
 window.addEventListener("load", function() {org.mozdev.dictionarysearch.init()},false);
